389e6ee5fc9e8c1f9928bd6bfcd0d7d7f31df550
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-10-21 00:10:38 -0700

    azimov

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Routers from "./router";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+function App() {
+    return (
+        <>
+            <Routers />
+        </>
+    );
+}
+
+export default App;
